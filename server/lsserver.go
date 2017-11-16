e3112337f229009cb3c93ddefa80cef9ac0a3efc
ziyuan <442367943@qq.com>
2017-11-16 19:14:43 +0800

    server complete

--- server/lsserver.go
+++ server/lsserver.go
@@ -155,4 +155,18 @@ func (lsServer *LsServer) handleConn(localConn *net.TCPConn) {
 		*/
 		lsServer.EncodeWrite(localConn, []byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
 	}
+
+	// 进行转发
+	// 从 localServer 读取数据发送到 dstServer
+	go func() {
+		err := lsServer.DecodeCopy(dstServer, localConn)
+		if err != nil {
+			// 在 copy 的过程中可能会存在网络超时等 error 被 return，只要有一个发生了错误就退出本次工作
+			localConn.Close()
+			dstServer.Close()
+		}
+	}()
+
+	// 从 dstServer 读取数据发送到 localUser，这里因为处在翻墙阶段出现网络错误的概率更大
+	lsServer.EncodeCopy(localConn, dstServer)
 }
